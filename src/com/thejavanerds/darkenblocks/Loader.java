f1824a6cc7997769c33c1b99f1868f5d5d4583c2
ZhsCraft <zhscraft@gmail.com>
2015-02-06 00:43:45 -0500

    Initial commit

---
+++ src/com/thejavanerds/darkenblocks/Loader.java
@@ -0,0 +1,13 @@
+package com.thejavanerds.darkenblocks;
+
+import java.applet.Applet;
+
+/**
+ * Created by ZhsCraft on 2/6/2015.
+ */
+public class Loader extends Applet {
+
+    public static void main(String[] args) {
+
+    }
+}
